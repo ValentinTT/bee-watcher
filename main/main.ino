dcc3239d7f57ba0214886a0c6412bb8fb67d77ff
ValentinTT <valentintapiatorti@gmail.com>
2018-08-13 17:56:47 -0300

    First Sleep Mode Implementation (UNO-noNano)
    This time the sleep mode was tested with an Arduino UNO, instead of a
    nano, because of the absence of a protoboard.
    It is inportant to remark that I had to struggle a lot to find out a
    problem which log was: "undefined reference to main" and "collect2.exe:
    error: ld returned 5 exit status". The problem seemed to be that the
    file was created with the Arduino IDE installe on C:/... but then I was
    trying to compile it with the portable IDE.
    The

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,34 @@
+#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
+#define interruptPin 2 //Pin we are going to use to wake up the Arduino
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  delay(5000);
+  goingToSleep();
 }
+
+void goingToSleep() {
+  sleep_enable();
+  attachInterrupt(0, wakeUp, LOW);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Going To Sleep");
+  delay(1000);
+  sleep_cpu();
+  Serial.println("Just Woke up");
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void wakeUp() {
+  Serial.println("Interrupt fired");
+  sleep_disable();//Disable sleep mode
+  detachInterrupt(0);
+}
+
