fe65f4205d3a90b9290e38a57ea8dadebad7c982
ValentinTT <valentintapiatorti@gmail.com>
2018-08-14 21:05:16 -0300

    First implementation of void readADXL
    This function read the ADXL335 sensor 10 times. It takes about 1 second
    to run the function, but its purpose is to avoid interference, so if
    more than 5 times the mesure was't normal it means the hive is moving.
    In the future the function is expected to return a boolean, instead of
    the LED_BUILTIN HIGH/LOW, to tell the micro if it's necessary to send an
    alarm to the beekeepr.

--- main/main.ino
+++ main/main.ino
@@ -1,34 +1,44 @@
-#include <avr/sleep.h>//this AVR library contains the methods that controls the sleep modes
-#define interruptPin 2 //Pin we are going to use to wake up the Arduino
 
+
+//Analog pins for the ADXL335 
+const byte xPin = 0;
+const byte yPin = 1;
+const byte zPin = 2;
+//Sensibility for the ADXL335
+const byte XYSensibility = 15; //Higher difference allowed between x and y axes
+const byte ZXYSensibility = 50; //Higher difference allowed between x-z axes or y-z axes
+//Variables for the ADXL335 reading
+short xValue;
+short yValue;
+short zValue;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-  delay(5000);
-  goingToSleep();
+  readADXL();
 }
 
-void goingToSleep() {
-  sleep_enable();
-  attachInterrupt(0, wakeUp, LOW);
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Going To Sleep");
-  delay(1000);
-  sleep_cpu();
-  Serial.println("Just Woke up");
-  digitalWrite(LED_BUILTIN, HIGH);
+void readADXL() {
+  byte bentCounter = 0; 
+  for(byte i = 0; i < 15; i++){ 
+    xValue = analogRead(xPin);
+    yValue = analogRead(yPin);
+    zValue = analogRead(zPin);
+    delay(100);
+    
+    if(abs(xValue - yValue) > XYSensibility) {
+      if(abs(xValue - zValue) > ZXYSensibility ||
+        abs(yValue - zValue) > ZXYSensibility) {
+        bentCounter++;
+      }    
+    }
+  }
+
+  if(bentCounter > 5) digitalWrite(LED_BUILTIN, HIGH);
+  else digitalWrite(LED_BUILTIN, LOW);
 }
 
-void wakeUp() {
-  Serial.println("Interrupt fired");
-  sleep_disable();//Disable sleep mode
-  detachInterrupt(0);
-}
+
 
