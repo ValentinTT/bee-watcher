58142b13547e3af52fcaae00f9935ad4664681fd
ValentinTT <valentintapiatorti@gmail.com>
2018-08-18 22:52:36 -0300

    First attempt of sleep mode with rtc and interruptions
    The sleep mode was implemented with the library avr/sleep.h. The RTC and
    their interruption was added with the library from JChristen
    DS3232RTC.h.
    The time for the RTC module is set using the __DATE__ and __TIME__
    macros from c++.

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,13 @@
+//Library for the controlling the sleep mode
+#include <avr/sleep.h>
+//Library for ds3231 with interruptions
+#include <DS3232RTC.h> //genera un objeto global RTC
 //Library necessary for the rf transmitter
 #include <VirtualWire.h>
 //Library for the DHT 22 sensor
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
+
 //Struct for sending data 
 #define hiveSafe 3
 #define hiveDangerFume 1
@@ -16,7 +21,7 @@ struct package {
 };
 struct package Data;
 
-//Pin used for transmission
+//Pin used for rf transmission
 const byte transmissionPin = 4;
 
 //DHT creation
@@ -41,7 +46,36 @@ const byte echoPin = 7;
 //Time/soundConstant = distance in centimeters
 const byte soundConstant = 58;
 
+//Pin to attach interruption, and interruption number bind to that pin
+const byte interruptionPin = 2;
+const byte interruptionNumber = 0;
+byte interruptAtMinute = 5;
+
 void setup() {
+  /**The RTC must be the first thing to initialize to try to be as accurate as possible
+   * with the compilation time, which is the time used to set the RTC time. 
+   * In case the gap between times is too much, there will be use other mechanism to set
+   * the RTC time
+   */
+  // Initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
+  RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
+  RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
+  RTC.alarm(ALARM_1);
+  RTC.alarm(ALARM_2);
+  RTC.alarmInterrupt(ALARM_1, false);
+  RTC.alarmInterrupt(ALARM_2, false);
+  RTC.squareWave(SQWAVE_NONE);
+
+  // set the RTC time and date to the compile time
+  RTC.set(compileTime());
+  // set Alarm 1 to occur at 0 seconds 5 minutes after every hour
+  RTC.setAlarm(ALM1_MATCH_MINUTES, 0, interruptAtMinute, 0, 1);
+  // clear the alarm flag
+  RTC.alarm(ALARM_1);
+  RTC.alarmInterrupt(ALARM_1, true);
+
+  pinMode(interruptionPin, INPUT_PULLUP);
+  
   //Transmitter configuration
   vw_set_tx_pin(transmissionPin);
   vw_set_ptt_inverted(true);
@@ -56,10 +90,62 @@ void setup() {
 }
 
 void loop() {
-  hiveEntranceOpening()
-  delay(2000);
+  delay(100);
+  goToSleep();
 }
 
+/* void goToSleep()
+ * This function put the microcontroller on sleep mode to save energy.
+ * The only way to wake the arduino up is to fire the interruption (connected to the RTC
+ * module) which takes the code to the function wakeUp.
+ */
+ void goToSleep() {
+  sleep_enable();
+  attachInterrupt(interruptionNumber, wakeUp, LOW);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  time_t t = RTC.get();
+  delay(100);
+  sleep_cpu();
+  t = RTC.get();
+  RTC.setAlarm(ALM1_MATCH_MINUTES, 0, interruptAtMinute, 0, 1);
+  RTC.alarm(ALARM_1);
+ }
+
+/* void wakeUp()
+ * This function is the ISR, in charge of disable the sleep mode on the microcontroller.
+ * It also changes the time in which the next alarm should be set.
+ */
+void wakeUp() {
+  sleep_disable();
+  interruptAtMinute += 5;
+  if(interruptAtMinute >= 60) interruptAtMinute -= 60;
+  detachInterrupt(interruptionNumber);
+}
+
+/* time_t compileTime() By JChristen 
+ * This function return a time_t object necesary to set the time in the RTC to the compilation
+ * time, using the values saved by C++ in __DATE__ and __TIME__ 
+ */
+ time_t compileTime() {
+  const time_t FUDGE(10);    //fudge factor to allow for upload time, etc. (seconds, YMMV)
+  const char *compDate = __DATE__, *compTime = __TIME__, *months = "JanFebMarAprMayJunJulAugSepOctNovDec";
+  char compMon[3], *m;
+
+  strncpy(compMon, compDate, 3);
+  compMon[3] = '\0';
+  m = strstr(months, compMon);
+
+  tmElements_t tm;
+  tm.Month = ((m - months) / 3 + 1);
+  tm.Day = atoi(compDate + 4);
+  tm.Year = atoi(compDate + 7) - 1970;
+  tm.Hour = atoi(compTime);
+  tm.Minute = atoi(compTime + 3);
+  tm.Second = atoi(compTime + 6);
+
+  time_t t = makeTime(tm);
+  return t + FUDGE;        //add fudge factor to allow for compile time
+}
 
 /* void sendData()
  * This function send the Data package(temp, hum, hiveEntrance) to the Main Unit after all the
